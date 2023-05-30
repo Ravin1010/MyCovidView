24d2cd841443a145da741b40a71fd10a6c636659
Lok17 <rotabite987@gmail.com>
2023-05-30 19:06:42 +0800

    Updated pages/index.ts

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as Overview } from "./Overview";
 export { default as Feedback } from "./Feedback";
 export { default as G11StackedBarChart } from "./group11/StackedBarChart";
 export { default as SunburstPage } from "./SunburstPage";
+export { default as TreemapPage } from "./TreeMapPage";
